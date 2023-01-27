22ed9deebd65cd1ada3014b9a5e39dfb4207bf0d
Your Name <njj64@hotmail.com>
2023-01-27 16:36:09 +0800

    basic project

---
+++ app/src/main/java/sg/nus/iss/team7/locum/APICommunication/ApiMethods.java
@@ -0,0 +1,16 @@
+package sg.nus.iss.team7.locum.APICommunication;
+
+
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiMethods {
+
+
+//    @POST("/api/customer/login")
+//    Call<CustomerDTO> loginCustomer(@Body CustomerDTO customerDTO);
+//
+//    @POST("/api/customer/register")
+//    Call<CustomerDTO> registerNewCustomer(@Body CustomerDTO customerDTO);
+
+}
---
+++ app/src/main/java/sg/nus/iss/team7/locum/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package sg.nus.iss.team7.locum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Use a thread to delay the splash screen for 2.5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                // close splash activity
+                finish();
+            }
+        }, 2500); // 2.5 seconds delay
+    }
+
+
+
+
+}
