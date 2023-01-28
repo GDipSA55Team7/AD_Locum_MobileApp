12a0e445b19253ea230a920b73705537a42b09fa
Your Name <njj64@hotmail.com>
2023-01-29 00:24:55 +0800

    getAbsoluteAdapterPosition() cannot resolve

--- app/src/main/java/sg/nus/iss/team7/locum/Adapter/JobSearchAdapter.java
+++ app/src/main/java/sg/nus/iss/team7/locum/Adapter/JobSearchAdapter.java
@@ -44,7 +44,7 @@ public class JobSearchAdapter extends RecyclerView.Adapter<JobSearchAdapter.MyVi
                 public void onClick(View v) {
                     if (recyclerViewInterface != null) {
                         int pos = getAbsoluteAdapterPosition();
-
+                        //int pos = getAdapterPosition();
                         if(pos != RecyclerView.NO_POSITION) {
                             recyclerViewInterface.onItemClick(pos);
                         }
